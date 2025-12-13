f0b5a66049f9badb229b6c46842a78fd8cc0b6fb
sasholilov <alex_xld@abv.bg>
2025-12-13 12:46:20 +0200

    remove db.json and use api.npoint

--- src/services/tasksApi.ts
+++ src/services/tasksApi.ts
@@ -1,8 +1,8 @@
-const BASE_URL = "https://tasks.free.beeceptor.com";
+const BASE_URL = "https://api.npoint.io/55d2a96ad4842d6fba44";
 
 export const getTasks = async () => {
   try {
-    const response = await fetch(`${BASE_URL}/tasks`);
+    const response = await fetch(`${BASE_URL}`);
     if (!response.ok) {
       throw new Error("Failed to fetch tasks");
     }
